86887f0636db026caa0e120b86e303d6aa786e32
Clinton Imaro <93707197+clintonimaroo@users.noreply.github.com>
2022-11-11 16:10:08 +0100

    Update Remove.ps1

---
+++ Remove.ps1
@@ -0,0 +1,11 @@
+$deleteProfiles = Get-WmiObject Win32_UserProfile | ? {
+    $_.LocalPath -notlike "C:\Windows*" 
+        } | Select PSComputerName, LocalPath, LastUseTime, SID | Out-GridView -PassThru -Title "Select profiles to delete"
+if ($deleteProfiles) {
+    Write-Output "The following profiles will be deleted:"
+    $deleteProfiles | % { $_.LocalPath }
+    Read-Host "Press ENTER to delete, or CTRL+C to exit"
+    Get-WmiObject Win32_UserProfile | ? { ($_.SID -in $deleteProfiles.SID) -and ($_.LocalPath -in $deleteProfiles.LocalPath) } | % { $_.Delete() }
+} else {
+    Write-Output "No profiles selected"
+}
